5160e74d9bccfaaba498786b39f6878dc7c15893
Junior Sundar <juniorsundar@gmail.com>
2025-03-11 10:18:58 +0400

    Update catppuccin-cyberdream.js

--- themes/catppuccin-cyberdream.js
+++ themes/catppuccin-cyberdream.js
@@ -1,1 +1,4 @@
-
+t.prefs_.set('color-palette-overrides', ["#363940", "#d96666", "#5eff6c", "#f1ff5e", "#4a90e2", "#ff5ea0", "#5ef1ff", "#ffd1dc", "#bd5eff", "#d96666", "#5eff6c", "#f1ff5e", "#4a90e2", "#ff5ea0", "#5ef1ff", "#ffffff"]);
+t.prefs_.set('foreground-color', "#ffffff");
+t.prefs_.set('background-color', "#16181a");
+t.prefs_.set('cursor-color', "#ff9f9f");
